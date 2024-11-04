ac6f27d9d994d262495438e04794240d7eb63b65
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2024-11-04 17:50:22 +0530

    Time: 1087 ms (5.13%) | Memory: 30 MB (24.06%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        int n = word.length();
+        string comp = "";
+        for(int i=0;i<n;i++){
+            int cnt = 1;
+            while(cnt <= 8 && i+1 < n && word[i] == word[i+1]){
+                cnt++;
+                i++;
+            }
+            comp += to_string(cnt) + word[i];
+            cout << word[i] << endl;
+        }
+        return comp;
+    }
+};
