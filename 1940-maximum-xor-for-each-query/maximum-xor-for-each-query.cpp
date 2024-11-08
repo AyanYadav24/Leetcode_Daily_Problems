fd638ee725f82918f27d101ff696116909400547
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2024-11-08 22:36:16 +0530

    Time: 0 ms (100.00%) | Memory: 99.9 MB (16.72%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
@@ -4,16 +4,14 @@ public:
         int n = nums.size();
         vector<int> ans;
         int xr = 0;
-        
-        int mask = (1 << maximumBit) - 1; //set bits of n size
+
+        int mask = (1 << maximumBit) - 1; // set bits of n size
         for (int i = 0; i < n; i++) {
             xr ^= nums[i];
         }
-        int i = 0;
-        while (i < n) {
-            ans.push_back(xr ^ mask); // for maximizing xor we need flip of givrn number
-            xr ^= nums[n-1-i];
-            i++;
+        for (int i = 0; i < n; i++) {
+            ans.push_back(xr ^ mask); // for maximizing xor we need flip of given number
+            xr ^= nums[n - 1 - i];
         }
         return ans;
     }
