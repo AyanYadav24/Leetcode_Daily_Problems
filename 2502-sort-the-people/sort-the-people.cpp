42f98d10022c1dc746a71fbc7715a0cdde983a0f
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2024-11-11 02:08:27 +0530

    Time: 17 ms (40.17%) | Memory: 27.2 MB (41.84%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        int n = names.size();
+        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
+        int n = names.size();
         vector<pair<int, string>> comb;
         for (int i = 0; i < n; i++) {
             comb.emplace_back(heights[i], names[i]);
@@ -17,3 +18,8 @@ public:
         return ans;
     }
 };
+
+auto init = []() {
+    
+    return 'c';
+}();
