623b843e854747b834c1db430bf029b7c1f21e64
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2024-12-01 21:23:24 +0530

    Time: 7 ms (7.25%) | Memory: 13.4 MB (24.36%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -3,11 +3,12 @@ public:
     bool checkIfExist(vector<int>& arr) {
         int n = arr.size();
         bool ans = false;
+        sort(arr.begin(),arr.end());
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(j!=i && arr[i] == 2 * arr[j]) ans = true;
+            for(int j=i+1;j<n;j++){
+                if(2 * arr[i] == arr[j] || arr[i] == 2 * arr[j]) ans = true;
             }
         }
-    return ans;
+       return ans;
     }
 };
