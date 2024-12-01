e0d2d10c115508f2e8ec3f4189ec949ce856f351
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2024-12-01 21:38:55 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (15.21%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        for (int i = 0; i < arr.size(); ++i) {
-            for (int j = 0; j < arr.size(); ++j) {
-                if (i == j) {
-                    continue;
-                }
-                if (arr[i] == arr[j] * 2) {
-                    return true;
-                }
-            }
+        unordered_map<int,bool> mp;
+        for(int i=0;i<arr.size();i++){
+            if(mp.count(arr[i] * 2) ||  ((arr[i] % 2 == 0) && mp.count(arr[i]/2))) return true;
+            mp[arr[i]] = true;
         }
         return false;
     }
