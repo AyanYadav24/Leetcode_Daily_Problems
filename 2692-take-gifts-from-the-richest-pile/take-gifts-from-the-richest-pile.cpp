0509fa15340eb93b8565718219ca7d1d9c492463
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2024-12-12 18:12:58 +0530

    Time: 26 ms (5.15%) | Memory: 13 MB (78.35%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int> pq;
+
+        for(int i=0;i<gifts.size();i++) pq.push(gifts[i]);
+
+        while(k--){
+            long long maxNum = pq.top();
+            cout << maxNum << endl;
+            pq.pop();
+            pq.push(floor(sqrt(maxNum)));
+        }
+
+        long long sum = 0;
+        while(!pq.empty()){
+            sum += pq.top();
+            pq.pop();
+        }
+        return sum;
+    }
+};
