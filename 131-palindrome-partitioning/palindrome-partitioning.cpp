0ad3418e09046a7abba4d440804bd17a7c03edaf
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2024-12-22 20:50:23 +0530

    Time: 40 ms (74.32%) | Memory: 57.5 MB (63.10%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+     bool isPalindrome(int i, int j, string s){
+        while(i <= j){
+            if(s[i] != s[j]) return false;
+            i++;j--;
+        }
+        return true;
+    }
+
+    void solve(int ind, string& s, vector<string>& temp, vector<vector<string>>& ans){
+        if(ind == s.length()){
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i=ind; i<s.length(); i++){
+            if(isPalindrome(ind, i , s)){
+                temp.push_back(s.substr(ind,i-ind+1));
+                solve(i+1,s,temp,ans);
+                temp.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<string> temp;
+        vector<vector<string>> ans;
+        solve(0,s,temp,ans);
+    return ans;
+    }
+};
