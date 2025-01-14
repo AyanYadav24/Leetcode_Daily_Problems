77c7532d07bdb79efa4928ac17375d4e7afaf97d
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-01-14 21:54:41 +0530

    Time: 8 ms (53.09%) | Memory: 107.6 MB (31.60%) - LeetSync

--- 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.cpp
+++ 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.cpp
@@ -3,31 +3,30 @@ public:
     int waysToMakeFair(vector<int>& nums) {
         int n = nums.size();
         int cnt = 0;
+        vector<int> oddSum(n,0);
+        vector<int> evenSum(n,0);
 
-        int evenTotalSum = 0, oddTotalSum = 0;
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) evenTotalSum += nums[i];
-            else oddTotalSum += nums[i];
+        evenSum[0] = nums[0];
+        for(int i=1;i<n;i++){
+            if(i%2==0){
+                evenSum[i] = evenSum[i-1] + nums[i];
+                oddSum[i] = oddSum[i-1];
+            }
+            else{
+                oddSum[i]  = oddSum[i-1] + nums[i];
+                evenSum[i] = evenSum[i-1];
+            }
         }
+        // for(int i=0;i<n;i++){
+        //     cout << "For index "<< i << " -> " << "even : " << evenSum[i] << " odd : " << oddSum[i] << endl;
+        // }
+        for(int i=1;i<n;i++){
+            int total_evenSum = evenSum[i-1] + oddSum[n-1] - oddSum[i];
+            int total_oddSum = oddSum[i-1] + evenSum[n-1]  -  evenSum[i];
 
-        int evenLeftSum = 0, oddLeftSum = 0;
-
-        for (int i = 0; i < n; i++) {
-           if(i%2 == 0)
-            evenTotalSum -= nums[i];
-           else
-            oddTotalSum -= nums[i];
-
-           if(evenLeftSum + evenTotalSum == oddLeftSum + oddTotalSum) cnt++;
-
-           if(i%2 == 0)
-            evenTotalSum -= nums[i];
-           else
-            oddTotalSum -= nums[i];
-           
-
+            if(total_evenSum == total_oddSum) cnt++;
         }
-
-        return cnt;
+        if(evenSum[n-1] - nums[0] == oddSum[n-1]) cnt++;
+    return cnt;
     }
 };
