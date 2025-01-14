dd4061ce954e0aea557ea9add0e9c8ecffa9a4ba
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-01-14 21:58:27 +0530

    Time: 4 ms (70.03%) | Memory: 107.6 MB (31.60%) - LeetSync

--- 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.cpp
+++ 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.cpp
@@ -20,11 +20,12 @@ public:
         // for(int i=0;i<n;i++){
         //     cout << "For index "<< i << " -> " << "even : " << evenSum[i] << " odd : " << oddSum[i] << endl;
         // }
+        
         for(int i=1;i<n;i++){
             int total_evenSum = evenSum[i-1] + oddSum[n-1] - oddSum[i];
             int total_oddSum = oddSum[i-1] + evenSum[n-1]  -  evenSum[i];
 
-            if(total_evenSum == total_oddSum) cnt++;
+            if(total_evenSum == total_oddSum) cnt++;//check for fair array condition
         }
         if(evenSum[n-1] - nums[0] == oddSum[n-1]) cnt++;
     return cnt;
