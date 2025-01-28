732691ec5a9ade745079a163654b250f735bcbe7
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-01-29 02:38:06 +0530

    Time: 8 ms (61.66%) | Memory: 95.9 MB (68.91%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    int dfs(int row, int col, vector<vector<int>>& vis,
-            vector<vector<int>>& grid, int m, int n) {
-        if (row < 0 || col < 0 || row >= m || col >= n || grid[row][col] == 0 || vis[row][col] == 1) return 0;
-        
-        vis[row][col] = 1;
+    int dfs(int row, int col, vector<vector<int>>& grid, int m, int n) {
+        if (row < 0 || col < 0 || row >= m || col >= n || grid[row][col] == 0) return 0;
+
         int curr_ans = grid[row][col];
+        grid[row][col] = 0;
 
         for (auto& d : dir) {
             int new_row = row + d[0];
             int new_col = col + d[1];
 
-            curr_ans += dfs(new_row, new_col, vis, grid, m, n);
+            curr_ans += dfs(new_row,new_col,grid, m, n);
         }
         return curr_ans;
     }
@@ -21,11 +20,10 @@ public:
         int n = grid[0].size();
         int ans = 0;
 
-        vector<vector<int>> vis(m, vector<int>(n, 0));
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] != 0 && vis[i][j] == 0)
-                    ans = max(ans, dfs(i, j, vis, grid, m, n));
+                if (grid[i][j] != 0)
+                    ans = max(ans, dfs(i, j, grid, m, n));
             }
         }
         return ans;
