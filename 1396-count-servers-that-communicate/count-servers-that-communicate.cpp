602941e9a148bab2fae81c41529e0c73bc908cfb
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-01-30 18:04:19 +0530

    Time: 0 ms (100.00%) | Memory: 26.8 MB (13.64%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<vector<int>> directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
     
-    void dfs(int row, int col, vector<vector<int>>& grid, vector<vector<bool>>& visited, int& cnt) {
+    void dfs(int row, int col, vector<vector<int>>& grid,int& cnt) {
         int m = grid.size();
         int n = grid[0].size();
-        if (row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == 0 || visited[row][col]) return;
+        if (row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == 0) return;
         
-        visited[row][col] = true;
+        grid[row][col] = 0;
         cnt++;
         
         for (auto& dir : directions) {
             int new_row = row + dir[0];
             int new_col = col + dir[1];
-            dfs(new_row, new_col, grid, visited, cnt);
+            dfs(new_row, new_col, grid, cnt);
         }
     }
     
@@ -21,10 +21,8 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<int> row_count(m, 0), col_count(n, 0);
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
         int totalServers = 0;
 
-        // Count the number of servers in each row and column
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
@@ -34,12 +32,11 @@ public:
             }
         }
 
-        // DFS to count the servers that can communicate
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1 && !visited[i][j]) {
+                if (grid[i][j] == 1) {
                     int cnt = 0;
-                    dfs(i, j, grid, visited, cnt);
+                    dfs(i, j, grid, cnt);
                     if (cnt > 1 || row_count[i] > 1 || col_count[j] > 1) {
                         totalServers += cnt;
                     }
