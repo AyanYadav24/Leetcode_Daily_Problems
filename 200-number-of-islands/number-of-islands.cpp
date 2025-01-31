f79c4efc06ea23429f04e59a5ddb6ed2cc1d5d67
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-01-31 16:13:07 +0530

    Time: 26 ms (65.09%) | Memory: 16.3 MB (67.46%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,24 +1,23 @@
 class Solution {
 private:
     vector<vector<int>> directions = {{-1,0},{0,-1},{1,0},{0,1}};
-    void dfs(int r, int c, vector<vector<char>>& mat, vector<vector<int>>& vis){
-        if(r<0 || c <0 || r>=mat.size() || c>=mat[r].size() || mat[r][c]=='0' || vis[r][c] == 1) return;
-        vis[r][c] = 1;
+    void dfs(int r, int c, vector<vector<char>>& mat){
+        if(r<0 || c<0 || r>=mat.size() || c>=mat[r].size() || mat[r][c]=='0') return;
+        mat[r][c] = '0';
        for(auto& dir : directions){
-        int nr = r + dir[0];
-        int nc = c + dir[1];
-        dfs(nr,nc,mat,vis);
+            int nr = r + dir[0];
+            int nc = c + dir[1];
+            dfs(nr,nc,mat);
        }
     }
 public:
     int numIslands(vector<vector<char>>& mat) {
-        vector<vector<int>> vis(mat.size(),vector<int>(mat[0].size(),0));
         int cnt = 0;
         for(int i=0;i<mat.size();i++){
             for(int j=0;j<mat[i].size();j++){
-                if(!vis[i][j] && mat[i][j] == '1'){
+                if(mat[i][j] == '1'){
                     cnt++;
-                    dfs(i,j,mat,vis);
+                    dfs(i,j,mat);
                 }
             }
         }
