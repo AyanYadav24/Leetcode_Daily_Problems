0ded2e300c5ad0752aca8bdaa14755ec0ec03d96
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-01-31 15:42:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (83.23%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,34 +1,19 @@
 class Solution {
 private:
-    void dfs(int node, vector<vector<int>>& adjList, vector<int>& vis){
+    void dfs(int node, vector<vector<int>>& mat, vector<int>& vis){
         vis[node] = 1;
-        for(auto it : adjList[node]){
-            if(!vis[it])
-                dfs(it,adjList,vis);
+        for(int i=0;i<mat.size();i++){
+            if(mat[node][i] && !vis[i])
+                dfs(i,mat,vis);
         }
     }
 public:
     int findCircleNum(vector<vector<int>>& mat) {
-        vector<vector<int>> adjList(mat.size());
-        for(int i=0;i<mat.size();i++){
-            for(int j=0;j<mat[0].size();j++){
-                if(mat[i][j] && i!=j){
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
-                }
-            }
-        }
-        for(auto i : adjList){
-            for(auto j : i){
-                cout << j << " ";
-            }
-            cout << endl;
-        }
-        vector<int> vis(adjList.size(),0);
+        vector<int> vis(mat.size(),0);
         int cnt = 0;
-        for(int i=0;i<adjList.size();i++){
+        for(int i=0;i<mat.size();i++){
             if(!vis[i]){
-                dfs(i,adjList,vis);
+                dfs(i,mat,vis);
                 cnt++;
             }
         }
