4f1a0e92e8d515700c81079c75469231c877a736
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-02-01 17:24:38 +0530

    Time: 86 ms (27.79%) | Memory: 112.8 MB (67.73%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
         int m = isWater.size();
         int n = isWater[0].size();
-        queue<pair<int,int>> q;
-        vector<vector<int>> heights(m, vector<int>(n, -1));
+        queue<pair<int, int>> q;
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-               if(isWater[i][j] == 1){
-                 q.push({i,j});
-                 heights[i][j] = 0;
-               }
+                if (isWater[i][j] == 1) {
+                    isWater[i][j] = 0;
+                    q.push({i, j});
+                } else {
+                    isWater[i][j] = -1;
+                }
             }
         }
-        while(!q.empty()){
-           pair<int,int> node = q.front();
-           q.pop();
-           int x = node.first;
-           int y = node.second;
-           for(auto& dir : directions){
-            int nx = x + dir[0];
-            int ny = y + dir[1];
-            if(nx >=0 && nx < m && ny >= 0 && ny < n && heights[nx][ny] == -1){
-                heights[nx][ny] = heights[x][y] + 1;
-                q.push({nx,ny});
+
+        while (!q.empty()) {
+            pair<int, int> node = q.front();
+            q.pop();
+            int x = node.first;
+            int y = node.second;
+            
+            for (auto& dir : directions) {
+                int nx = x + dir[0];
+                int ny = y + dir[1];
+
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && isWater[nx][ny] == -1) {
+                    isWater[nx][ny] = isWater[x][y] + 1;
+                    q.push({nx, ny});
+                }
             }
-           } 
         }
-        return heights;
+
+        return isWater;
     }
 };
