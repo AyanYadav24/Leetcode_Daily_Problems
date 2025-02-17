c8a4427e1916368619d8264d8a5482db5b962219
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-02-17 23:43:17 +0530

    Time: 1 ms (88.19%) | Memory: 8 MB (84.16%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    void allSubstrings(int ind, string& s, vector<string>& ans, string& temp) {
-        if (ind < 0) {
-            ans.push_back(temp);
-            return;
-        }
-        temp.push_back(s[ind]);
-        allSubstrings(ind - 1, s, ans, temp);
-        while (ind > 0 && s[ind] == s[ind - 1])
-            ind--;
-        temp.pop_back();
-        allSubstrings(ind - 1, s, ans, temp);
-    }
-    void permute(int ind, string s, set<string>& uniquePermutes) {
-        if (ind == s.size()) {
-            uniquePermutes.insert(s);
-            return;
-        }
-        for (int i = ind; i < s.length(); i++) {
-            swap(s[i], s[ind]);
-            permute(ind + 1, s, uniquePermutes);
-            swap(s[i], s[ind]);
-        }
-    }
-    int numTilePossibilities(string tiles) {
-        int n = tiles.size();
-        vector<string> ans;
-        set<string> uniquePermutes;
-        string temp = "";
-        sort(tiles.begin(),tiles.end());
-        allSubstrings(n-1,tiles,ans,temp);
+    int total;
+    void findSequences(vector<int>& count) {
+        total++;
 
-        for (auto it : ans) {
-            permute(0, it, uniquePermutes);
+        for (int pos = 0; pos < 26; pos++) {
+            if (count[pos] == 0) {
+                continue;
+            }
+
+            count[pos]--;
+            findSequences(count);
+            count[pos]++;
         }
-        return uniquePermutes.size() - 1;
+    }
+
+    int numTilePossibilities(std::string tiles) {
+        total = 0;
+
+        vector<int> count(26, 0);
+        for (char c : tiles) {
+            count[c - 'A']++;
+        }
+
+        findSequences(count);
+        return total-1;
     }
 };
