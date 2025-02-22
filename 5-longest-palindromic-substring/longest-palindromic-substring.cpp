a53282e39dca217e3be32d069862fca8e9f28a6d
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-02-22 22:59:35 +0530

    Time: 103 ms (38.53%) | Memory: 13.8 MB (59.29%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -3,6 +3,7 @@ public:
     int t[1001][1001];
     bool isPalindrome(int i, int j, string& s) {
     if(i>=j) return 1;
+    if(t[i][j] != -1) return t[i][j];
     if(s[i] == s[j]) return t[i][j] = isPalindrome(i+1,j-1,s);
     return t[i][j] = 0;
 }
