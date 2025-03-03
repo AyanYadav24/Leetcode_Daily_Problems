fe647a0aadcb4ed8ee3df1a2d8cc877098de4162
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-03-03 20:51:44 +0530

    Time: 2 ms (19.57%) | Memory: 9.9 MB (93.62%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> ans;
+    string temp;
+
+    void solve(int idx, string& s, set<string>& st) {
+        if (idx >= s.length()) {
+            ans.push_back(temp);
+            return;
+        }
+        
+        for (int i = idx; i < s.length(); i++) {
+            string c = s.substr(idx, i - idx + 1);
+            if (st.find(c) != st.end()) {
+                if (idx != 0) temp += " " + c;
+                else temp += c;
+                solve(i + 1, s, st);
+                if (idx != 0) temp.pop_back();
+                for (int j = 0; j < c.length(); j++) temp.pop_back();
+            }
+        }
+    }
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        set<string> st(wordDict.begin(), wordDict.end());
+        solve(0, s, st);
+        return ans;
+    }
+};
