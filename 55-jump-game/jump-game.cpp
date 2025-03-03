c8e59cb1997a7cff52c7c2968a9ce4b05ae10cab
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-03-03 13:17:27 +0530

    Time: 16 ms (18.91%) | Memory: 54.4 MB (10.87%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    vector<int> dp;
-    bool solve(int ind, vector<int>& nums, int n){
-        if(ind == n - 1) return true;
-        if(dp[ind] != -1) return dp[ind];
-
-        for(int i = 1; i <= nums[ind]; i++) {
-            if(ind + i < n && solve(ind + i, nums, n))
-                return dp[ind] = true;
-        }
-        return dp[ind] = false;
-    }
-
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1) return true;
-        dp.resize(n, -1);
-        return solve(0, nums, n);
+        priority_queue<int, vector<int>, greater<int>> jmps;
+
+        if(nums.size() == 1) return true;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            while(!jmps.empty() && jmps.top() < i) jmps.pop();
+
+            if(jmps.empty() && i != 0) return false;
+
+            jmps.push(nums[i] + i);
+        }
+
+        return true;
     }
 };
