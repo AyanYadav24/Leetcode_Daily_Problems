968c4a724cdd367e59c5d0549a4e72e948b1e872
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-03-03 20:03:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (80.94%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    bool solve(int idx, string& s, set<string>& st, vector<int>& dp) {
-        if (idx >= s.length()) return true;
-        if (dp[idx] != -1) return dp[idx];
-
-        for (int i = idx; i < s.length(); i++) {
-            if (st.find(s.substr(idx, i - idx + 1)) != st.end()) {
-                if (solve(i + 1, s, st, dp)) {
-                    return dp[idx] = true;
-                }
-            }
-        }
-        return dp[idx] = false;
-    }
-
     bool wordBreak(string s, vector<string>& wordDict) {
-        set<string> st(wordDict.begin(), wordDict.end());
-        vector<int> dp(s.length(), -1);
-        return solve(0, s, st, dp);
+      set<string> set(wordDict.begin(),wordDict.end());
+      
+      int n=s.size();
+      vector<bool> v(n+1, false);
+      v[0]=true;
+        for(int i=1;i<=n;i++) {
+          for(string w : wordDict) {
+            int start=i-w.size();
+            if(start>=0 && v[start] && s.substr(start,w.size())==w) {
+              v[i]=true;
+              break;
+            }
+          }
+        }
+        return v[n];
+        
     }
 };
