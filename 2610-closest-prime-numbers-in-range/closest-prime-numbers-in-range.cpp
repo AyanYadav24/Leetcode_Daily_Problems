77af9f7bc32133ed6a9697943904103b8cc80b7f
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-03-08 01:33:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (96.60%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,53 +1,38 @@
 class Solution {
 public:
-    vector<int> closestPrimes(int left, int right) {
-        int upperLimit = right;
-        vector<int> sieveArray = sieve(upperLimit);
-
-        vector<int> primeNumbers;  
-        for (int num = left; num <= right; num++) {
-            if (sieveArray[num] == 1) {
-                primeNumbers.push_back(num);
-            }
+    bool isPrime(int n) {
+        if (n <= 1) 
+            return false;
+        if (n == 2) 
+            return true;
+        if (n % 2 == 0) 
+            return false;
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) 
+                return false;
         }
-
-        if (primeNumbers.size() < 2)
-            return vector<int>{-1, -1}; 
-
-        int minDifference = INT_MAX;
-        vector<int> closestPair(2, -1);
-
-        for (int index = 1; index < primeNumbers.size(); index++) {
-            int difference = primeNumbers[index] - primeNumbers[index - 1];
-            if(difference <= 2){
-                closestPair[0] = primeNumbers[index - 1];
-                closestPair[1] = primeNumbers[index];
-                return closestPair;
-            }
-            if (difference < minDifference) {
-                minDifference = difference;
-                closestPair[0] = primeNumbers[index - 1];
-                closestPair[1] = primeNumbers[index];
-            }
-        }
-        return closestPair;
+        return true;
     }
 
-private:
-    vector<int> sieve(int upperLimit) {
-        vector<int> sieve(upperLimit + 1,1);
-
-        sieve[0] = 0;
-        sieve[1] = 0;
-
-        for (int number = 2; number * number <= upperLimit; number++) {
-            if (sieve[number] == 1) {
-                for (int multiple = number * number; multiple <= upperLimit;
-                     multiple += number) {
-                    sieve[multiple] = 0;
+    vector<int> closestPrimes(int left, int right) {
+        int a = -1, b = -1;
+        int minDiff = INT_MAX;
+        int prev = -1;
+        
+        for (int i = left; i <= right; i++) {
+            if (isPrime(i)) {
+                if (prev != -1) {
+                    int diff = i - prev;
+                    if(diff <= 2 ) return {prev,i};
+                    if (diff < minDiff) {
+                        minDiff = diff;
+                        a = prev;
+                        b = i;
+                    }
                 }
+                prev = i;
             }
         }
-        return sieve;
+        return {a, b};
     }
 };
