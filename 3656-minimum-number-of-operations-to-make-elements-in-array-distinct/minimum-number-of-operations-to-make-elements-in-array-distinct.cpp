2997ba020e9bd55db301dab33580f6cfec724739
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-04-08 15:58:08 +0530

    Time: 4 ms (45.39%) | Memory: 27.4 MB (90.61%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,30 +1,10 @@
 class Solution {
 public:
-    bool unique(vector<int>& hash) {
-        for (int i = 0; i < hash.size(); i++) {
-            if (hash[i] > 1) return false;
-        }
-        return true;
-    }
-    
     int minimumOperations(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> hash(101, 0);
-        
-        for (int num : nums)
-            hash[num]++;
-        
-        int ans = 0;
-        int idx = 0;
-
-        while (!unique(hash)) {
-            int count = 3;
-            while (idx < n && count-- > 0) {
-                hash[nums[idx]]--;
-                idx++;
-            }
-            ans++;
-        }
-    return ans;
+        vector<int> mpp(101);
+        for (int i = nums.size() - 1; i >= 0; --i)
+            if (++mpp[nums[i]] > 1) 
+                return (i + 3) / 3;
+        return 0;
     }
 };
