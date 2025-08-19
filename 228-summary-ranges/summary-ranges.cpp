a55f57bfb7f11ab0f0fc09a43e1ddf297d53ca17
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-08-19 16:49:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (41.40%) - LeetSync

--- 228-summary-ranges/summary-ranges.cpp
+++ 228-summary-ranges/summary-ranges.cpp
@@ -7,7 +7,6 @@ public:
         while(i<n){
             int p = nums[i];
             while(i < n-1 && nums[i] + 1 == nums[i+1]) i++;
-            cout << p << " " << nums[i] << endl;
             string s = "";
             if(p==nums[i]) s = to_string(p);
             else s = to_string(p) + "->" + to_string(nums[i]);
