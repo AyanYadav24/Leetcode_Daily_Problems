7a1d105f6d75735c171ffe51831dcdad0750a633
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-08-19 14:20:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (10.75%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s = to_string(num);
+        int i = 0;
+        while( i < s.length() && s[i] == '9') {
+            i++;
+        }
+        if(i<s.length()) s[i] = '9';
+        int n = stoi(s);
+        return n;
+    }
+};
