ca286e37c5fc941f1d3124ee7bfadc9d2c834021
Ayan Yadav <121786875+AyanYadav24@users.noreply.github.com>
2025-08-29 15:17:47 +0530

    Time: 2 ms (35.93%) | Memory: 8.8 MB (66.70%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
-    }
-
-    bool backtrack(const string& digits, vector<bool>& used, int curr, int count) {
-        if (count == digits.size()) return isPowerOfTwo(curr);
-
-        for (int i = 0; i < digits.size(); i++) {
-            if (used[i]) continue;
-
-            if (curr == 0 && digits[i] == '0') continue;
-
-            int newNum = curr * 10 + (digits[i] - '0');
-            used[i] = true;
-
-            if (backtrack(digits, used, newNum, count + 1)) return true;
-
-            used[i] = false;
+    bool reorderedPowerOf2(int n) {
+        string s = sortString(to_string(n));
+        for (int i = 0; i < 31; ++i) {
+            string powerStr = sortString(to_string(1 << i));
+            if (s == powerStr) return true;
         }
         return false;
     }
-
-    bool reorderedPowerOf2(int n) {
-        string digits = to_string(n);
-        vector<bool> used(digits.size(), false);
-
-        return backtrack(digits, used, 0, 0);
+    
+    string sortString(string s) {
+        sort(s.begin(), s.end());
+        return s;
     }
 };
